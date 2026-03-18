48bd6656cadbe65aeab4e34835c036fb92799d3c
Gracias Claude <gracias.claude@outlook.com>
2026-03-17 22:54:36 -0600

    chore: fix issue where gaming wasn't restarting

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,25 +3,34 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("This is the start of a guess game \n Please enter in a guess: ");
+    println!("This is the start of a guess game \n\n");
     // check the user input
-    let mut guess: String = String::new();
+    let random_number: i32 = rand::thread_rng().gen_range(1..=100);
 
     // random number
-    let mut random_number: i32 = rand::thread_rng().gen_range(1..=100);
-    // what does  &mut do in this case?
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to  read line");
+    loop {
+        let mut guess: String = String::new();
+        println!("Please enter in a guess: ");
 
-    // it's probably the parse
-    let guess: i32 = guess.trim().trim().parse().expect("Please enter a number!");
-    println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to  read line");
 
-    match guess.cmp(&random_number) {
-        Ordering::Less => println!("The number you guess is low"),
-        Ordering::Greater => println!("The number you guessed is to high"),
-        Ordering::Equal => println!("You got it"),
+        // it's probably the parse
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+        match guess.cmp(&random_number) {
+            Ordering::Less => println!("The number you guess is low"),
+            Ordering::Greater => println!("The number you guessed is to high"),
+            Ordering::Equal => {
+                println!("You got it!");
+                break;
+            }
+        }
     }
     println!("The secret number is: {}", random_number);
 }
